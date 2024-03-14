2ce4e02b8866cb0bd0399ac5ce79a19e5da5bd5a
Panagiotis Tsakas <panagiotis.tsakas2@gmail.com>
2024-03-15 00:25:49 +0100

    processed data - template 1 - basic processing of raw data

---
+++ processed_data/template_1/run.py
@@ -0,0 +1,131 @@
+'''Basic processing of raw data.
+Store b-values, b-vectors, and median_otsu mask.
+'''
+
+
+import os
+import pickle
+import logging
+from typing import Protocol
+from datetime import datetime
+from dataclasses import dataclass
+from argparse import ArgumentParser
+
+import numpy as np
+import nibabel as nib
+from dipy.segment.mask import median_otsu
+
+
+class RawDataPaths(Protocol):
+    b_values_file: str
+    b_vectors_file: str
+    raw_data_file: str
+
+
+@dataclass
+class ProcessedDataHyperparameters:
+    raw_data_paths_pkl: str
+
+
+@dataclass
+class ProcessedDataPaths:
+    experiment_path = os.path.join(
+        'processed_data', 'template_1', 'experiments', 
+        datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    
+    def __post_init__(self):
+        os.makedirs(self.experiment_path)
+
+    @property
+    def log_file(self):
+        return os.path.join(self.experiment_path, 'log.txt')
+    
+    @property
+    def hyperparameters_file(self):
+        return os.path.join(self.experiment_path, 'hparams.pkl')
+    
+    @property
+    def b_values_file(self):
+        return os.path.join(self.experiment_path, 'bvals.npy')
+
+    @property
+    def b_vectors_file(self):
+        return os.path.join(self.experiment_path, 'bvecs.npy')
+    
+    @property
+    def mask_file(self):
+        return os.path.join(self.experiment_path, 'mask.npy')
+
+
+def main():
+
+    ## PROCESSED DATA PATHS
+    
+    proc_data_paths = ProcessedDataPaths()
+
+
+    ## LOGGING
+
+    logging.basicConfig(
+        filename=proc_data_paths.log_file,
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+    ## PROCESSED DATA HYPERPARAMETERS
+
+    parser = ArgumentParser()
+    parser.add_argument('--raw_data_paths_pkl', type=str, required=True)
+    args = parser.parse_args()
+
+    proc_data_hparams = ProcessedDataHyperparameters(**vars(args))
+
+    with open(proc_data_paths.hyperparameters_file, 'wb') as f:
+        pickle.dump(proc_data_hparams, f)
+    
+    logging.info('Processed data hyperparameters:')
+    for key, value in vars(proc_data_hparams).items():
+        logging.info(f'{key}: {value}')
+    logging.info('')
+
+
+    ## RAW DATA PATHS
+
+    with open(proc_data_hparams.raw_data_paths_pkl, 'rb') as f:
+        raw_data_paths: RawDataPaths = pickle.load(f)
+
+    logging.info('Raw data paths:')
+    for key, value in vars(raw_data_paths).items():
+        logging.info(f'{key}: {value}')
+    logging.info('')
+
+
+    ## B-VECTORS
+
+    b_vectors = np.genfromtxt(raw_data_paths.b_vectors_file)
+
+    np.save(proc_data_paths.b_vectors_file, b_vectors)
+
+
+    ## B-VALUES
+
+    b_values = np.genfromtxt(raw_data_paths.b_values_file)
+    
+    b_values[(b_values > 9_900) & (b_values < 10_100)] = 10_000
+    b_values /= 1_000.0
+
+    np.save(proc_data_paths.b_values_file, b_values)
+
+
+    ## DIFF DATA
+    
+    raw_data  = nib.load(raw_data_paths.raw_data_file).get_fdata() # type: ignore
+    
+    # use b-value = 0.0 for stronger brain signal to get the mask
+    masked_data, mask = median_otsu(raw_data, vol_idx=np.where(b_values == 0.0)[0])
+
+    np.save(proc_data_paths.mask_file, mask)
+
+
+if __name__ == '__main__':
+    main()
