14b410edd10c503fd4f2948ee148c53c58ec70c6
Panagiotis Tsakas <panagiotis.tsakas2@gmail.com>
2024-03-25 16:40:34 +0100

    data analysis - template 1 - analyze processed data for range, zeros and negative values

--- data_analysis/template_1/run.py
+++ data_analysis/template_1/run.py
@@ -1,6 +1,6 @@
-'''Load raw data.
-Load processed b-values and median_otsu mask.
-Analyze masked data range per b-value in histograms.
+'''Load processed b-values and data.
+Analyze data per b-value and for all b-values combined:
+min, max, num_zeros, num_neg, total.
 Option to zero-mean b=0.0 images.
 Save histograms.'''
 
@@ -14,22 +14,12 @@ from dataclasses import dataclass
 from argparse import ArgumentParser
 
 import numpy as np
-import nibabel as nib
 import matplotlib.pyplot as plt
 
 
-class RawDataPaths(Protocol):
-    raw_data_file: str
-
-
-class ProcessedDataHyperparameters(Protocol):
-    raw_data_paths_pkl: str
-
-
 class ProcessedDataPaths(Protocol):
-    hyperparameters_file: str
     b_values_file: str
-    mask_file: str
+    processed_data_file: str
 
 
 @dataclass
@@ -40,7 +30,7 @@ class DataAnalysisHyperparameters:
 
 class DataAnalysisPaths:
     def __init__(self) -> None:
-        self.experiment_path = os.path.join('raw_data_analysis', 'template_1.1', 'experiments', 
+        self.experiment_path = os.path.join('data_analysis', 'template_1', 'experiments', 
                                             datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
         self.log_file = os.path.join(self.experiment_path, 'log.txt')
         self.paths_file = os.path.join(self.experiment_path, 'paths.pkl')
@@ -93,72 +83,53 @@ def main():
     with open(data_analysis_hparams.processed_data_paths_pkl, 'rb') as f:
         proc_data_paths: ProcessedDataPaths = pickle.load(f)
 
-
-    ## PROCESSED DATA HYPERPARAMETERS
-
-    with open(proc_data_paths.hyperparameters_file, 'rb') as f:
-        proc_data_hparams: ProcessedDataHyperparameters = pickle.load(f)
-
-
-    ## RAW DATA PATHS
-
-    with open(proc_data_hparams.raw_data_paths_pkl, 'rb') as f:
-        raw_data_paths: RawDataPaths = pickle.load(f)
-    
-    
-    ## RAW DATA
-        
-    raw_data  = nib.load(raw_data_paths.raw_data_file).get_fdata() # type: ignore
-
     
     ## B-VALUES
     
-    b_values = np.load(proc_data_paths.b_values_file)
+    b_values: np.ndarray = np.load(proc_data_paths.b_values_file)
 
-    b_value_to_indices = {b_value: np.where(b_values == b_value)[0] for b_value in np.unique(b_values)}
+    b_value_to_indices: dict[float, np.ndarray] = {b_value: np.where(b_values == b_value)[0] for b_value in np.unique(b_values)}
     
     
-    ## MASKED DATA
+    ## PROCESSED DATA
 
-    mask = np.load(proc_data_paths.mask_file)
-
-    masked_data = raw_data * mask[..., None]
+    processed_data: np.ndarray = np.load(proc_data_paths.processed_data_file)
 
     
     ## MASKED SIGNAL RANGE ANALYSIS
 
     fig, ax = plt.subplots(1 + len(b_value_to_indices), 1, figsize=(10, 6), sharex=True)
-    fig.suptitle('Masked Signal Intensity Histograms per b-value')
+    fig.suptitle('Signal Intensity Histograms per b-value')
 
     for i, (b_value, indices) in enumerate(b_value_to_indices.items()):
 
         print(f'Processing b-value: {b_value}')
         
-        S = masked_data[:, :, :, indices]
+        S = processed_data[:, indices]
 
         if b_value == 0.0 and data_analysis_hparams.b_zero_mean:
-            S = S.mean(axis=-1)
+            S = S.mean(axis=1)
             b_value = '0.0 (mean)'
         
-        min_val, max_val = np.min(S), np.max(S)    
-        logging.info(f'b-value: {b_value} \t min: {min_val} \t max: {max_val}')    
+        min_val, max_val, num_zeros, num_neg = np.min(S), np.max(S), np.sum(S == 0.0), np.sum(S < 0.0)
+        logging.info(f'b-value: {b_value:<10} min: {min_val:<10.1f} max: {max_val:<10.1f} num_zeros: {num_zeros:<10} num_neg: {num_neg:<10} total: {S.size:<10}')
         
-        ax[i].hist(S[S != 0.0].flatten(), bins=1000, histtype='step', label=f'b-value: {b_value}')
+        ax[i].hist(S.flatten(), bins=1000, histtype='step', label=f'b-value: {b_value}')
         ax[i].legend()
         ax[i].yaxis.set_visible(False)
         ax[i].axvline(min_val, color='r', linestyle='--')
         ax[i].axvline(max_val, color='g', linestyle='--')
 
-    min_val, max_val = np.min(masked_data), np.max(masked_data)
-    logging.info(f'b-value: ALL \t min: {min_val} \t max: {max_val}')
+    min_val, max_val, num_zeros, num_neg = np.min(processed_data), np.max(processed_data), np.sum(processed_data == 0.0), np.sum(processed_data < 0.0)
+    logging.info(f'b-value: {"ALL":<10} min: {min_val:<10.1f} max: {max_val:<10.1f} num_zeros: {num_zeros:<10} num_neg: {num_neg:<10} total: {processed_data.size:<10}')
 
-    ax[-1].hist(masked_data[masked_data != 0.0].flatten(), bins=1000, histtype='step', label=f'b-value: ALL')
+    ax[-1].hist(processed_data.flatten(), bins=1000, histtype='step', label=f'b-value: ALL')
     ax[-1].legend()
     ax[-1].yaxis.set_visible(False)
     ax[-1].axvline(min_val, color='r', linestyle='--')
     ax[-1].axvline(max_val, color='g', linestyle='--')
 
-    ax[-1].set_xlabel('Masked Signal Intensity')
+    ax[-1].set_xlabel('Signal Intensity')
     plt.savefig(data_analysis_paths.histograms_file)
 
 
